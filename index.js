3443824fc01a1930798baa4e66e12a7671d02855
Rishi <email4rishi@fastmail.com>
2022-08-29 15:17:53 +1000

    Video 3 map operator-JSX
    Video 3 map operator-JSX

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+import { testMap } from './map.js';
+
+
 console.log("Its working");
+
+const showMapbutton = document.querySelector("#mapButton");
+showMapbutton.addEventListener('click', () => {
+    console.log("Map operator clicked");
+    testMap();
+
+})
